21552a6958fdbebc286514591565c3652cbbd0b1
dinhnam_ddn <namanhdev.7.nn@gmail.com>
2021-12-02 21:40:18 +0700

    added play button and tutorial button

--- src/scenes/home-scene.js
+++ src/scenes/home-scene.js
@@ -6,15 +6,20 @@ export default class HomeScene extends Phaser.Scene{
     }
 
     preload(){
-        this.load.image('background', 'assets/circus.png');
-        this.load.spritesheet('player', 'assets/player.png', {frameWidth: 16, frameHeight: 32});
-        this.load.audio('background-sound', 'assets/backgroundsound.mp3');
+        this.load.image('background', './assets/circus.png');
+        this.load.image('button', './assets/button.png');
+        this.load.spritesheet('player', './assets/player.png', {frameWidth: 16, frameHeight: 32});
+        this.load.audio('background-sound', './assets/backgroundsound.mp3');
     }
 
     create(){
         this.add.image(0, 0, 'background').setScale(2).setOrigin(0, 0);
-        var background_sound = this.sound.add('background-sound', {volume: 0.2, loop: true}).play();
+        this.bg_sound = this.sound.add('background-sound');
+        this.bg_sound.play({volume: 0.2, loop: true});
 
+        this.playButton = this.add.image(300, 120, 'button').setScale(0.5).setOrigin(0, 0);
+        this.tutorialButton = this.add.image(505, 120, 'button').setScale(0.5).setOrigin(0, 0);
+        
         this.anims.create({
             key: 'anims-player',
             frameRate: 10,
@@ -25,12 +30,17 @@ export default class HomeScene extends Phaser.Scene{
         this.add.sprite(382, 275, null).setScale(3).play('anims-player');
         this.add.sprite(482, 275, null).setScale(3).play('anims-player');
         this.add.sprite(582, 275, null).setScale(3).play('anims-player');
-        this.add.text(420, 150, 'PLAY', {fontSize: '50px', fill: '#ffffff'});
-    }
+        this.add.text(350, 146, 'PLAY', {font: 'bold 20px sana-serif', fill: '#ffffff', align: 'center'});
+        this.add.text(530, 146, 'TUTORIAL', {font: 'bold 20px sana-serif', fill: '#ffffff', align: 'center'});
 
-    update(){
-        if(this.input.activePointer.leftButtonDown()) {
+        this.playButton.setInteractive();
+        this.playButton.on('pointerdown', ()=>{
+            this.bg_sound.stop();
             this.scene.start('GamePlay');
-        }
+        })
+        this.tutorialButton.setInteractive();
+        this.tutorialButton.on('pointerover', ()=>{
+            console.log('hello')
+        })
     }
 }
