3035e73478ed8d497ad6e21e4b487a8d56a4412e
dinhnam_ddn <namanhdev.7.nn@gmail.com>
2021-12-04 23:18:01 +0700

    change font on button

--- src/scenes/tutorial-scene.js
+++ src/scenes/tutorial-scene.js
@@ -13,7 +13,7 @@ export class TutorialScene extends Phaser.Scene {
         
         this.backButton = this.add.image(419, 280, 'button').setScale(0.4).setOrigin(0, 0);
 
-        this.add.text(445, 300, 'CLOSE', {font: 'bold 20px sana-serif', fill: '#ffffff', align: 'center'});
+        this.add.text(450, 298, 'CLOSE', {font: 'bold 22px consolas', fill: '#ffffff', align: 'center'});
 
         this.backButton.setInteractive();
         this.backButton.on('pointerdown', ()=>{
