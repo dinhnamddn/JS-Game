43b9c77c57f1ca3052cd9faa748b32b2c884cc56
vinhshe150155 <vinhhshe150155@fpt.edu.vn>
2021-12-08 13:21:43 +0700

    Add bruh lmao trap

--- src/main.js
+++ src/main.js
@@ -2,17 +2,17 @@ import HomeScene from "./scenes/home-scene.js";
 import { GamePlay } from "./scenes/game-play.js";
 import { TutorialScene } from "./scenes/tutorial-scene.js";
 const config = {
-    type: Phaser.AUTO,
-    width: 960,//
-    height: 384,//
-    backgroundColor: '#ffffff',
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 300 },
-            debug: true
-        }
+  type: Phaser.AUTO,
+  width: 960, //
+  height: 384, //
+  backgroundColor: "#ffffff",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: true,
     },
-    scene: [HomeScene, GamePlay, TutorialScene]
+  },
+  scene: [HomeScene, GamePlay, TutorialScene],
 };
 var game = new Phaser.Game(config);
