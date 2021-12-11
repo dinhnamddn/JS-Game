4f9d35c5d77efc7dbdeeaca5751f8748df21b03f
dinhnam_ddn <namanhdev.7.nn@gmail.com>
2021-12-11 09:51:11 +0700

    change intro text

--- src/scenes/intro.js
+++ src/scenes/intro.js
@@ -18,9 +18,9 @@ export class Intro extends Phaser.Scene {
         this.closeButton = this.add.image(420, 220, 'button').setScale(0.4).setOrigin(0, 0);
 
         this.add.text(447, 237, 'READY', {font: 'bold 25px consolas', fill: '#ffffff', align: 'center'});
-        this.add.text(110, 120, "Một chú hề muốn rời khỏi rạp xiếc.\n" 
+        this.add.text(110, 120, "Một chú hề muốn tham gia vào rạp xiếc JS Club.\n" 
             + "Nhưng rạp xiếc muốn giữ chú hề ở lại và đã tạo ra một thử thách cực kỳ ảo lòi và khó nhằn,\n" 
-            + "nếu như chú hề có thể vượt qua thử thách đó thì cậu ta mới được cho phép rời khỏi rạp xiếc.\n" 
+            + "nếu như chú hề có thể vượt qua thử thách đó thì cậu ta mới được cho phép tham gia rạp xiếc.\n" 
             + "Với kỹ năng đỉnh cao của mình, bạn hãy điều khiển để giúp chú hề chiến thắng thử thách", 
             {
                 font: 'bold 15px consolas', 
